c89314a69849cb205c3b599cf44705f968b285bf
Ricardo Alf <rear_recinos@hotmail.com>
2024-12-16 10:19:34 -0600

    refactor: Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  type OpenAPIObject,
+} from '@nestjs/swagger';
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory: () => OpenAPIObject = () =>
+    SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
